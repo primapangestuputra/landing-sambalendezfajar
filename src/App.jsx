742a0c1e14e8fa24bc6d7e874d5ac71481d718d4
Prima Pangestu Putra <primapangestuputra@gmail.com>
2026-01-29 14:35:53 +0700

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div className="w-full h-screen bg-green-200">Lorem</div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+    return (
+        <>
+            <div className=""></div>
+        </>
+    );
+};
+
+export default Hero;
