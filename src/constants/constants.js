6eb25d93500d788fac6d44c7a9013c904d43e6cd
Prima Pangestu Putra <primapangestuputra@gmail.com>
2026-02-02 14:57:09 +0700

    feat: add products data to constants

---
+++ src/constants/constants.js
@@ -0,0 +1,41 @@
+import FirstImage from "../assets/images/hero-img.jpg";
+import SecondImage from "../assets/images/card-img.jpeg";
+
+export const products = [
+    {
+        name: "Signature Cumi Endez",
+        price: "Rp 25.000",
+        description:
+            "Varian primadona dengan potongan cumi segar yang melimpah dan kenyal. Bumbu pedasnya meresap sempurna, memberikan kepuasan maksimal di setiap suapan.",
+        firstImage: FirstImage,
+        secondImage: SecondImage,
+        href: "#",
+    },
+    {
+        name: "Teri Medan Premium",
+        price: "Rp 20.000",
+        description:
+            "Gurihnya teri Medan pilihan yang tetap renyah dalam balutan sambal autentik. Dibuat tanpa pengawet untuk menjaga kesegaran rasa di meja makan Anda.",
+        firstImage: FirstImage,
+        secondImage: SecondImage,
+        href: "#",
+    },
+    {
+        name: "Petai Rempah Melimpah",
+        price: "Rp 22.000",
+        description:
+            "Sensasi petai besar yang garing dipadukan dengan racikan rempah yang medok. Pilihan tepat bagi Anda yang mendambakan rasa yang kuat dan berkarakter.",
+        firstImage: FirstImage,
+        secondImage: SecondImage,
+        href: "#",
+    },
+    {
+        name: "Terasi Udang Autentik",
+        price: "Rp 18.000",
+        description:
+            "Aroma terasi udang bakar yang mewah dengan profil rasa pedas-gurih yang seimbang. Sambal klasik yang ditingkatkan kualitasnya ke level premium.",
+        firstImage: FirstImage,
+        secondImage: SecondImage,
+        href: "#",
+    },
+];
